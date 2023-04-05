e5800e50cd8a2bf68a08702603507ccb4d7a2750
Harry Patlow <ghp.coding@gmail.com>
2023-04-05 18:59:46 +0530

    deploy

--- workerantdjango/settings.py
+++ workerantdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9h%p8tp#jbp=e0#+embu)ff2-lqbdmn3_(qfwp2r6!mn6^31az
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
